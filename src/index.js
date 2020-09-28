67fc481ca3401872846670c9064ad34d5d337d7b
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-09-28 00:17:56 -0600

    add configuration of jwt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     ApiConfig: require("./api.config"),
     FirebaseConfig: require("./firebase.config"),
+    JwtConfig: require("./jwt.config"),
     MongoConfig: require("./mongo.config"),
     PaginationConfig: require("./pagination.config"),
     SequelizeConfig: require("./sequelize.config")

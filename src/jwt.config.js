4ba7fe0e874551007b3311e71113ba4414717a38
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-09-28 13:51:52 -0600

    fix some variables that need an instance

--- src/jwt.config.js
+++ src/jwt.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    secret: process.env.JWT_SECRET.replace(/\\n/g, '\n'),
+    secret: process.env.JWT_SECRET?.replace(/\\n/g, '\n'),
     serviceAccountEmail:  process.env.JWT_SERVICE_ACCOUNT_EMAIL,
     expTime: Number(process.env.JWT_EXP_TIME)
 }

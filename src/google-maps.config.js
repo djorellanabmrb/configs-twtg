566fd08ef30fc312f5fb8fb203edbf45b85650c8
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-11-06 15:38:41 -0600

    master

--- src/google-maps.config.js
+++ src/google-maps.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    apiKey: process.env.GOOGLE_MAPS_API_KEY
+    apiKey: process.env.GOOGLE_MAPS_API_KEY,
+    url: "https://maps.googleapis.com",
+    geoCode: "/maps/api/geocode/json"
 }

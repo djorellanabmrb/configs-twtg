443cf9399855db559a52620aa6798b6b36997171
djorellanabmrb <70344779+djorellanabmrb@users.noreply.github.com>
2021-01-29 05:36:38 -0600

    Update server.config.js

--- src/server.config.js
+++ src/server.config.js
@@ -2,5 +2,9 @@ module.exports = {
     Api:{
         protocol: process.env.SERVER_API_PROTOCOL,
         hostname: process.env.SERVER_API_HOSTNAME
+    },
+    Frontend:{
+        protocol: process.env.SERVER_FRONTEND_PROTOCOL,
+        hostname: process.env.SERVER_FRONTEND_HOSTNAME
     }
 }
